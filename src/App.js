12adf4db8f066eb72c23f86e9f0ce00c7bd27a40
Eddie Espinosa <espinosa.edilson@gmail.com>
2022-11-02 16:26:30 -0400

    refactored css

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './scenes/Home';
 import SingleAlbum from './scenes/SingleAlbum';
 import AddAlbum from './scenes/AddAlbum';
-import './App.css';
+import './assets/master.css';
 
 function App() {
   return (
